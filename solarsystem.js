967e7abf4f744605331517962632947ad8bf2fca
clementine <cleriv.72@gmail.com>
2023-07-18 15:14:31 +0200

    Appel de l'API ok --> à exploiter

--- solarsystem.js
+++ solarsystem.js
@@ -6,6 +6,8 @@
 //         return data
 //     });
 // };
+
+
 // fetchPokemonInfo('snorlax')
 // .then(   pokemon => { 
 //     document.getElementById('pokemon-name').textContent = pokemon.name;
@@ -14,3 +16,27 @@
 //     document.getElementById('pokemon-abilities').textContent = abilities.join(', ');
 // })
 // .catch(error => console.error(error))
+
+// Cette fonction permet de récupérer un objet grace à l'API
+
+function fetchPlanetInfo(numero) {
+    return fetch(`https://swapi.dev/api/planets/${numero}`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            return data
+        });
+};
+
+// Cette fonction permet de récupérer les données provenant de l'API 
+
+fetchPlanetInfo('21')
+    .then(function(planet) {
+        console.log(planet)
+        document.getElementById('planet-name').textContent = planet.name;
+        document.getElementById('planet-climate').textContent = planet.climate;
+        document.getElementById('planet-diameter').textContent = planet.diameter + " km";
+        document.getElementById('planet-population').textContent = planet.population;
+    })
+    .catch(function(error){console.error(error)})
+
