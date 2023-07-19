c55cbaafc0c965e3bebe92f75831afe41db53cc1
clementine <cleriv.72@gmail.com>
2023-07-19 16:40:18 +0200

    Version appel à la fonction aec maj argument via bouton

--- solarsystem.js
+++ solarsystem.js
@@ -20,26 +20,19 @@
 // Cette fonction permet de récupérer un objet grace à l'API
 
 function fetchPlanetInfo(numero) {
+    console.log(numero)
     return fetch(`https://swapi.dev/api/planets/${numero}`)
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            return data
-        });
+        .then(function (response){return response.json()})
+        .then(function (planet){
+            console.log(planet)
+            document.getElementById('planet-name').textContent = planet.name;
+            document.getElementById('planet-climate').textContent = planet.climate;
+            document.getElementById('planet-diameter').textContent = planet.diameter + " km";
+            document.getElementById('planet-population').textContent = planet.population;
+        })
+        .catch(function (error) { console.error(error) })
 };
 
-// Cette fonction permet de récupérer les données provenant de l'API 
-
-
-fetchPlanetInfo('21')
-    .then(function(planet) {
-        console.log(planet)
-        document.getElementById('planet-name').textContent = planet.name;
-        document.getElementById('planet-climate').textContent = planet.climate;
-        document.getElementById('planet-diameter').textContent = planet.diameter + " km";
-        document.getElementById('planet-population').textContent = planet.population;
-    })
-    .catch(function(error){console.error(error)})
 
 
 
