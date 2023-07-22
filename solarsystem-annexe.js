2c434eea1965350d14a97e3e5fcf0d5426083ea3
clementine <cleriv.72@gmail.com>
2023-07-22 15:04:11 +0200

    ajout des logos élémentaires

--- solarsystem-annexe.js
+++ solarsystem-annexe.js
@@ -1,14 +1,14 @@
 const planetlist =
     [
-        { numero: "001", value: "1", nom: "Tatooine", type: "SABLE", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Tatooine.png"/>', resume: "à rédiger" },
-        { numero: "002", value: "2", nom: "Alderaan", type: "PLANTE", type2: "TERRE", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Alderaan.png"/>', resume: "à rédiger" },
-        { numero: "004", value: "4", nom: "Hoth", type: "GLACE", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Hoth.png"/>', resume: "à rédiger" },
-        { numero: "009", value: "6", nom: "Bespin", type: "AIR", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Bespin.jpg"/>', resume: "à rédiger" },
-        { numero: "013", value: "8", nom: "Naboo", type: "PLANTE", type2: "VILLE", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Naboo.jpg"/>' , resume: "à rédiger"},
-        { numero: "014", value: "9", nom: "Coruscant", type: "ACIER", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Coruscant.png"/>', resume: "à rédiger" },
-        { numero: "017", value: "10", nom: "Kamino", type: "EAU", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Kamino.jpg"/>', resume: "à rédiger" },
-        { numero: "020", value: "11", nom: "Geonosis", type: "ROCHE", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Geonosis.png"/>', resume: "à rédiger" },
-        { numero: "024", value: "21", nom: "Eriadu", type: "ACIER", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Eriadu.jpg"/>', resume: "à rédiger" },
-        { numero: "028", value: "13", nom: "Mustafar", type: "TENEBRE", type2: "FEU", icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Mustafar.png"/>', resume: "à rédiger" }
+        { numero: "001", value: "1", nom: "Tatooine", type: '<img id="type" src="Banque images/Logo éléments/Sable.png"/> SABLE', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Tatooine.png"/>', resume: "à rédiger" },
+        { numero: "002", value: "2", nom: "Alderaan", type: '<img id="type" src="Banque images/Logo éléments/Plante.png"/> PLANTE', type2: '<img id="type2" src="Banque images/Logo éléments/Terre.png"/> TERRE', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Alderaan.png"/>', resume: "à rédiger" },
+        { numero: "004", value: "4", nom: "Hoth", type: '<img id="type" src="Banque images/Logo éléments/Glace.png"/> GLACE', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Hoth.png"/>', resume: "à rédiger" },
+        { numero: "009", value: "6", nom: "Bespin", type: '<img id="type" src="Banque images/Logo éléments/Air.png"/> AIR', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Bespin.png"/>', resume: "à rédiger" },
+        { numero: "013", value: "8", nom: "Naboo", type: '<img id="type" src="Banque images/Logo éléments/Plante.png"/> PLANTE', type2: '<img id="type2" src="Banque images/Logo éléments/Métal.png"/> VILLE', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Naboo.png"/>' , resume: "à rédiger"},
+        { numero: "014", value: "9", nom: "Coruscant", type: '<img id="type" src="Banque images/Logo éléments/Métal.png"/> ACIER', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Coruscant.png"/>', resume: "à rédiger" },
+        { numero: "017", value: "10", nom: "Kamino", type: '<img id="type" src="Banque images/Logo éléments/Eau.png"/> EAU', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Kamino.png"/>', resume: "à rédiger" },
+        { numero: "020", value: "11", nom: "Geonosis", type: '<img id="type" src="Banque images/Logo éléments/Terre.png"/> ROCHE', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Geonosis.png"/>', resume: "à rédiger" },
+        { numero: "024", value: "21", nom: "Eriadu", type: '<img id="type" src="Banque images/Logo éléments/Métal.png"/> ACIER', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Eriadu.png"/>', resume: "à rédiger" },
+        { numero: "028", value: "13", nom: "Mustafar", type: '<img id="type" src="Banque images/Logo éléments/Ténèbre.png"> TENEBRE', type2: '<img id="type2" src="Banque images/Logo éléments/Feu.png"/> FEU', icone: "en attente", portrait: '<img src="Banque images/Portraits planètes/Mustafar.png"/>', resume: "à rédiger" }
     ]
 
